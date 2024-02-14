7fe239afd27e79c4c9cb57015d8bd6aa9970f5e6
Ashit Darurmath <ashitsdarurmath@gmail.com>
2024-02-15 01:36:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import MonthlyCalendar from "./components/calendar"
+
+
+function App() {
+
+  return (
+   <div>
+      <MonthlyCalendar></MonthlyCalendar>
+   </div>
+  )
+}
+
+
+
+export default App
